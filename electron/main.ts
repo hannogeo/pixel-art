555921178b41177c9fe0750fa8e5930d15ac9ca1
Hanno <249422871+hannogeo@users.noreply.github.com>
2026-05-09 19:25:32 +0300

    fix: surely this works now

--- electron/main.ts
+++ electron/main.ts
@@ -18,7 +18,7 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win: BrowserWindow | null
 
 function createWindow() {
-  const iconPath = path.join(process.env.VITE_PUBLIC, 'icon.png')
+  const iconPath = path.join(process.env.VITE_PUBLIC || '', 'icon.png')
   
   win = new BrowserWindow({
     icon: iconPath,
